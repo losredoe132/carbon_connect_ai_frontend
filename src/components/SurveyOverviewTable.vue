d48e42d2d7a2dc55d54096f02c323062aafadc03
Finn Jonas Peper <j.jonas.peper@gmail.com>
2025-04-17 09:23:00 +0000

    add survey overview

---
+++ src/components/SurveyOverviewTable.vue
@@ -0,0 +1,40 @@
+<template>
+    <q-table title="User List" :rows="rows" :columns="columns" row-key="id" :pagination="pagination"
+        @request="onRequest" @row-click="onRowClick" class="cursor-pointer">
+
+    </q-table>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const rows = ref([
+    { id: 1, name: 'Alice', age: 25, email: 'alice@example.com' },
+    { id: 2, name: 'Bob', age: 30, email: 'bob@example.com' },
+    { id: 3, name: 'Charlie', age: 28, email: 'charlie@example.com' }
+])
+
+const columns = [
+    { name: 'id', label: 'ID', field: 'id', sortable: true },
+    { name: 'name', label: 'Name', field: 'name', sortable: true },
+    { name: 'age', label: 'Age', field: 'age', sortable: true },
+]
+
+const pagination = ref({
+    sortBy: 'name',
+    descending: false,
+    page: 1,
+    rowsPerPage: 5
+})
+
+function onRequest(props) {
+    pagination.value = props.pagination
+}
+
+function onRowClick(row) {
+    router.push(`/survey/${row.id}`)
+}
+</script>
---
+++ src/pages/SurveyOverviewPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="q-pa-md">
+    <!-- Survey ID Heading -->
+    <h1 class="text-h5 q-mb-md">Survey Overview</h1>
+
+    <q-card>
+      <SurveyOverviewTable />
+    </q-card>
+    <!-- Expandable Meta Info Section -->
+  </q-page>
+</template>
+
+
+<script setup>
+import SurveyOverviewTable from 'components/SurveyOverviewTable.vue'
+</script>
+<style scoped>
+.q-img {
+  display: inline-block;
+  margin-right: 10px;
+}
+</style>
