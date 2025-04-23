f248f882e5e8ebc5f94b3adf8cff4805549f3189
Finn Jonas Peper <j.jonas.peper@gmail.com>
2025-04-23 17:20:07 +0000

    remove left drawer toggle window with click function

--- carbon_connect_ai/src/layouts/MainLayout.vue
+++ carbon_connect_ai/src/layouts/MainLayout.vue
@@ -3,40 +3,15 @@
 
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-
         <q-toolbar-title>
           Carbon Connect AI
         </q-toolbar-title>
-        <q-toolbar class="">
-          <div>Quasar v{{ $q.version }}</div>
-        </q-toolbar>
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-
-      <q-list>
-
-        <q-item>
-          <q-item-section>
-            <q-btn flat @click="$router.push('/survey')">
-              Survey Overview
-            </q-btn>
-          </q-item-section>
-        </q-item>
-
-        <q-item>
-          
-        </q-item>
 
 
 
-      </q-list>
-
-      <!-- drawer content -->
-
-    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -50,7 +25,4 @@ import { ref } from 'vue'
 
 const leftDrawerOpen = ref(false)
 
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value
-}
 </script>
