5a7e3512de3d9afeca3da8cca69823a24b033ab9
Finn Jonas Peper <j.jonas.peper@gmail.com>
2025-05-03 13:50:21 +0000

    add spinner while loading the data

--- carbon_connect_ai/src/pages/SurveyPage.vue
+++ carbon_connect_ai/src/pages/SurveyPage.vue
@@ -1,79 +1,88 @@
 <template>
   <q-page class="q-pa-md">
-    <!-- Survey ID Heading -->
     <div class="text-h5 q-mb-md">Survey ID: {{ survey_id }}</div>
-    <q-list>
-      <!-- Expandable Meta Info Section -->
+    <div v-if="is_loading" class="row justify-center q-my-md">
+      <q-spinner-dots size="40px" color="primary" />
+    </div>
+    <div v-else>
+      <q-list>
 
-      <q-card class="q-mb-md">
-        <SurveyMeta :survey_id="survey_id" :timestamp="timestamp" :lat="survey_lat" :lon="survey_lon"
-          :observer="observer_name" />
-      </q-card>
+        <q-card class="q-mb-md">
+          <SurveyMeta :survey_id="survey_id" :timestamp="timestamp" :lat="survey_lat" :lon="survey_lon"
+            :observer="observer_name" />
+        </q-card>
 
 
-      <q-card>
-        <q-expansion-item class="q-mb-md" expand-separator>
-          <template #header>
-            <div class="row">
-              <q-icon :name="'circle'" :color="biochar_images_valid ? 'green' : 'red'" />
-              <div>Biochar Images</div>
-            </div>
-          </template>
+        <q-card>
+          <q-expansion-item class="q-mb-md" expand-separator>
+            <template #header>
+              <div class="row">
+                <q-icon :name="'circle'" :color="biochar_images_valid ? 'green' : 'red'" />
+                <div>Biochar Images</div>
+              </div>
+            </template>
 
-          <q-card class="row">
-            <div v-for="item in biochar_images" v-bind:key="item.id">
-              <SurveyImageCard :item=item />
-            </div>
-          </q-card>
-        </q-expansion-item>
-      </q-card>
+            <q-card class="row">
+              <div v-for="item in biochar_images" v-bind:key="item.id">
+                <SurveyImageCard :item=item />
+              </div>
+            </q-card>
+          </q-expansion-item>
+        </q-card>
 
-      <q-card>
-        <q-expansion-item class="q-mb-md" expand-separator>
-          <template #header>
-            <div class="row">
-              <q-icon :name="'circle'" :color="biochar_images_valid ? 'green' : 'red'" />
-              <div>Moisture Images</div>
-            </div>
-          </template>
+        <q-card>
+          <q-expansion-item class="q-mb-md" expand-separator>
+            <template #header>
+              <div class="row">
+                <q-icon :name="'circle'" :color="biochar_images_valid ? 'green' : 'red'" />
+                <div>Moisture Images</div>
+              </div>
+            </template>
 
-          <q-card class="row">
-            <div v-for="item in moisture_images" v-bind:key="item.id">
-              <SurveyImageCard :item=item />
-            </div>
-          </q-card>
-        </q-expansion-item>
-      </q-card>
+            <q-card class="row">
+              <div v-for="item in moisture_images" v-bind:key="item.id">
+                <SurveyImageCard :item=item />
+              </div>
+            </q-card>
+          </q-expansion-item>
+        </q-card>
 
-      <q-card>
-        <q-expansion-item class="q-mb-md" expand-separator>
-          <template #header>
-            <div class="row">
-              <q-icon :name="'circle'" :color="biochar_images_valid ? 'green' : 'red'" />
-              <div>Bags Images</div>
-            </div>
-          </template>
+        <q-card>
+          <q-expansion-item class="q-mb-md" expand-separator>
+            <template #header>
+              <div class="row">
+                <q-icon :name="'circle'" :color="biochar_images_valid ? 'green' : 'red'" />
+                <div>Bags Images</div>
+              </div>
+            </template>
 
-          <q-card class="row">
-            <div v-for="item in bags_images" v-bind:key="item.id">
-              <SurveyImageCard :item=item />
-            </div>
-          </q-card>
-        </q-expansion-item>
-      </q-card>
+            <q-card class="row">
+              <div v-for="item in bags_images" v-bind:key="item.id">
+                <SurveyImageCard :item=item />
+              </div>
+            </q-card>
+          </q-expansion-item>
+        </q-card>
 
-    </q-list>
+      </q-list>
+    </div>
   </q-page>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue'
 import { api } from 'boot/axios'
+import { useRoute } from 'vue-router'
 import SurveyMeta from 'components/SurveyMeta.vue'
 import SurveyImageCard from 'components/SurveyImageCard.vue'
 
+const route = useRoute()
+const survey_id = route.params.surveyId
+
+const is_loading = ref(true)
+const data = ref(null)
+
 const survey_info = ref(null)
-const survey_id = ref('')
 const survey_lat = ref('')
 const survey_lon = ref('')
 const observer_name = ref('')
@@ -99,26 +108,35 @@ desired_values_moisture.value = {
 }
 
 const fetchData = async (surveyId) => {
-  const res = await api.get(`/survey/${surveyId}`)
-  const data = res.data
+  try {
+    const res = await api.get(`/survey/${surveyId}`)
+    data.value = res.data
+  }
+  catch (error) {
+    console.error('Failed to fetch data:', error);
+  }
+  finally {
+    is_loading.value = false
+  }
 
-  const survey_meta = data.survey_info.meta
 
-  survey_id.value = survey_meta.biochar_production_survey_id
+
+  const survey_meta = data.value.survey_info.meta
+
   survey_lat.value = survey_meta.survey_location.latitude
   survey_lon.value = survey_meta.survey_location.longitude
   observer_name.value = survey_meta.observer_name
   timestamp.value = survey_meta.collection_timestamp_utc
 
-  biochar_images.value = data.biochar_images
-  moisture_images.value = data.moisture_images
-  bags_images.value = data.bags_images
+  biochar_images.value = data.value.biochar_images
+  moisture_images.value = data.value.moisture_images
+  bags_images.value = data.value.bags_images
 
   biochar_images_valid.value = true
 }
 
 onMounted(() => {
-  const res = fetchData('70b69ccb-9fba-49b9-8d3c-5d755c6a17d7')
+  const res = fetchData(survey_id)
   survey_info.value = res.data
 })
 </script>
